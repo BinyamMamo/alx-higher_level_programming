90a418751b735f229d1d7c441487ea49e3a67e5b
BinyamMamo <binyammamo01@gmail.com>
2023-03-12 08:25:11 +0300

    Sun, Mar 12, 2023  8:25:10 AM

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python3
 
 def pow(a, b):
+    if b > 0:
+        return pow(a, b)
+    elif b == 0:
+        return 1
+    return 1 / powr(a, b * -1)
+
+
+def powr(a, b):
     if b < 1:
         return 1
-    return a * pow(a, b - 1)
+    return a * powr(a, b - 1)
+
+
+print(pow(2, 3))
+print(pow(2, -3))
+print(pow(1, 3))
+print(pow(1, -3))
+print(pow(1, 0))
