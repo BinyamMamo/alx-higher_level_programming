ac5520ea20946aee2c5962424192564b3247f556
BinyamMamo <binyammamo01@gmail.com>
2023-03-12 07:48:21 +0300

    Sun, Mar 12, 2023  7:48:21 AM

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,6 +3,6 @@
 def uppercase(str):
     for c in str:
         if ord(c) in range(ord("a"), ord("z") + 1):
-            print(chr(ord(c) - 32), end="")
-        else:
-            print(c, end="")
+            c = chr(ord(c) - 32)
+        print(c, end="")
+    print("")
