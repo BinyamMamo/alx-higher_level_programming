68706cdfb026a77dc3bcfcc6dfbf07757f23688f
BinyamMamo <binyammamo01@gmail.com>
2023-03-13 17:00:26 +0300

    Mon, Mar 13, 2023  5:00:25 PM

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -7,14 +7,3 @@ def print_matrix_integer(matrix=[[]]):
             if x.index(y) < len(x) - 1:
                 print(", ", end="")
         print("")
-
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-print_matrix_integer(matrix)
-print("--")
-print_matrix_integer()
