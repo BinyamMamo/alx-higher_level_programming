054307831b7ea9299cdf50e7db54853b0da7fdc1
BinyamMamo <binyammamo01@gmail.com>
2023-03-13 17:00:55 +0300

    Mon, Mar 13, 2023  5:00:55 PM

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,8 +2,3 @@
 
 def multiple_returns(sentence):
     return (len(sentence), sentence[0])
-
-
-sentence = "At school, I learnt C!"
-length, first = multiple_returns(sentence)
-print("Length: {:d} - First character: {}".format(length, first))
