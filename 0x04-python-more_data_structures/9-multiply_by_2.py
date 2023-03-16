8d51109990f62917ef4114aaa1f012a38c17c4bf
BinyamMamo <binyammamo01@gmail.com>
2023-03-16 16:55:34 +0300

    Thu, Mar 16, 2023  4:55:30 PM

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -4,9 +4,9 @@
 #     __import__('6-print_sorted_dictionary').print_sorted_dictionary
 
 
-def multiply_by_2(a_dictionary):
+def multiply_by_2(a_dictionary={}):
     if a_dictionary is None:
-        return a_dictionary
+        return None
     new_values = list(map(lambda x: x * 2, a_dictionary.values()))
     for key, new_val in zip(a_dictionary.keys(), new_values):
         a_dictionary[key] = new_val
