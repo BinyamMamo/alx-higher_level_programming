ab6f89c49be96909ddb125703d3a3f44675b01f9
BinyamMamo <binyammamo01@gmail.com>
2023-03-16 16:32:37 +0300

    Thu, Mar 16, 2023  4:32:33 PM

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -5,6 +5,8 @@
 
 
 def multiply_by_2(a_dictionary):
+    if a_dictionary is None:
+        return a_dictionary
     new_values = list(map(lambda x: x * 2, a_dictionary.values()))
     for key, new_val in zip(a_dictionary.keys(), new_values):
         a_dictionary[key] = new_val
