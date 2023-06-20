7c2c907051d0b0dae0d32e1f000a8e134880a484
BinyamMamo <binyammamo01@gmail.com>
2023-06-20 16:19:49 +0300

    Tue Jun 20 16:19:49 EAT 2023

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 let lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (var i = 0; i < lines.length; i++) {
+for (let i = 0; i < lines.length; i++) {
     console.log(lines[i]);
 }
