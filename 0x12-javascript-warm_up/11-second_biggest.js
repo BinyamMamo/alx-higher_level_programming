1e3fd09916eabe4aedb00686dd63e641571fdcbd
BinyamMamo <binyammamo01@gmail.com>
2023-06-20 17:32:46 +0300

    Tue Jun 20 17:32:46 EAT 2023

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,18 +1,18 @@
 #!/usr/bin/node
 const argc = process.argv.length;
-if (argc <= 4) {
+if (argc <= 3) {
   console.log(0);
 } else {
   let max = 0;
   let secMax = 0;
   for (let x = 2; x < argc; x++) {
-    if (process.argv[x] > max) {
-      max = process.argv[x];
+    if (parseInt(process.argv[x]) > max) {
+      max = parseInt(process.argv[x]);
     }
   }
   for (let x = 2; x < argc; x++) {
-    if (process.argv[x] > secMax && process.argv[x] !== max) {
-      secMax = process.argv[x];
+    if (parseInt(process.argv[x]) > secMax && parseInt(process.argv[x]) !== max) {
+      secMax = parseInt(process.argv[x]);
     }
   }
   console.log(secMax);
