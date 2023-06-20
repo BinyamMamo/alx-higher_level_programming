309eddc9731bd5a7f7132d7c6956f2e23694cd75
BinyamMamo <binyammamo01@gmail.com>
2023-06-20 16:33:56 +0300

    Tue Jun 20 16:33:56 EAT 2023

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,6 +3,6 @@ if (isNaN(process.argv[2]) || process.argv[2] === undefined || process.argv[2] =
   console.log('Missing number of occurrences');
 } else {
   for (let x = 0; x < parseInt(process.argv[2]); x++) {
-    console.log('x'.repeat(parseInt(process.argv[2])));
+    console.log('X'.repeat(parseInt(process.argv[2])));
   }
 }
