43c49f266c27dad75d47933c736163c674c0c164
BinyamMamo <binyammamo01@gmail.com>
2023-06-24 19:26:35 +0300

    Sat Jun 24 19:26:35 EAT 2023

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,10 @@
+#!/usr/bin.node
+exports.nbOccurences = function (list, searchElement) {
+  let occr = 0;
+  list.forEach(element => {
+    if (searchElement === element) {
+      occr += 1;
+    }
+  });
+  return (occr);
+}
