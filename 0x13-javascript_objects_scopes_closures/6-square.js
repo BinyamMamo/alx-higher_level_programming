1da00af47ee8d8580b21ab01059bafe2604f382a
BinyamMamo <binyammamo01@gmail.com>
2023-06-24 17:56:15 +0300

    Sat Jun 24 17:56:15 EAT 2023

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const SquareParent = require('./5-square');
+class Square extends SquareParent {
+  constructor (size) {
+    super(size);
+    this.size = size;
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let y = 0; y < this.size; y++) {
+      console.log('X'.repeat(this.size));
+    }
+  }
+}
+module.exports = Square;
