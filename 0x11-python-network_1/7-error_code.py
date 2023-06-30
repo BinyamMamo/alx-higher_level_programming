214d1153531e5fbae2025fcc70dd387d7212571a
BinyamMamo <117805811+BinyamMamo@users.noreply.github.com>
2023-06-30 20:27:47 +0000

    Fri Jun 30 20:27:47 UTC 2023

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """sends a POST request to the passed URL with the email as a parameter
+using the requests package
 
-Usage: ./3-error_code.py <URL>
+Usage: ./7-error_code.py <URL>
 """
 import sys
 import requests
 
 if __name__ == "__main__":
-    try:
-        with requests.get(sys.argv[1]) as resp:
+    with requests.get(sys.argv[1]) as resp:
+        if resp.status_code < 400:
             print(resp.read().decode("utf-8"))
-    except HTTPError as exception:
-        print(f'Error code: {exception.code}')
+        else:
+            print(f'Error code: {resp.status_code}')
