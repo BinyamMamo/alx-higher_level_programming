bf07ae9ebc689f97480196d0fe1b0cf55a802ded
BinyamMamo <binyammamo01@gmail.com>
2023-06-30 15:27:50 +0300

    Fri Jun 30 15:27:50 EAT 2023

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# parses the content length and displays it
-curl -sI $1 | grep "content-length" | cut -d " " -f 2
+# Get the byte size of the HTTP response header for a given URL.
+curl -s "$1" | wc -c
