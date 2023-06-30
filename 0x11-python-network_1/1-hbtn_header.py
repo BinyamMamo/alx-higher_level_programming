f588abcd0b972466d2d880f8854899c3aac165b7
BinyamMamo <binyammamo01@gmail.com>
2023-06-30 19:08:07 +0300

    Fri Jun 30 19:08:07 EAT 2023

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""fetches data from the URL provided in the first argument and displays X-Request-Id
+"""fetches data from the URL provided in the first argument 
+and displays X-Request-Id
+
 Usage: ./1-hbtn_header.py <URL>
 """
 from urllib import request
