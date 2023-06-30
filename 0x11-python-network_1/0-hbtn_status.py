203533bcce13e8ffafbadd449af36f82f7372435
BinyamMamo <binyammamo01@gmail.com>
2023-06-30 17:57:22 +0300

    Fri Jun 30 17:57:22 EAT 2023

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-  from urllib import request
-  resp = request.urlopen("https://alx-intranet.hbtn.io/status")
-  data = resp.read()
-  print("Body response:")
-  print(f"\t- type: {type(data)}")
-  print(f"\t- content: {data}")
-  print(f"\t- utf8 content: {data.decode('UTF-8')}")
+    from urllib import request
+    resp = request.urlopen("https://alx-intranet.hbtn.io/status")
+    data = resp.read()
+    print("Body response:")
+    print(f"\t- type: {type(data)}")
+    print(f"\t- content: {data}")
+    print(f"\t- utf8 content: {data.decode('UTF-8')}")
