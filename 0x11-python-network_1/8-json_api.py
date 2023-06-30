128131b8a5bce05e34cea121fd1a544deecef17d
BinyamMamo <117805811+BinyamMamo@users.noreply.github.com>
2023-06-30 21:14:39 +0000

    Fri Jun 30 21:14:39 UTC 2023

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,24 +1,13 @@
 #!/usr/bin/python3
-"""Sends a POST request to http://0.0.0.0:5000/search_user
-adding an input letter using the requests package
+"""takes GitHub credentials from the first and second
+argument and uses the GitHub API to display id
 
 Usage: ./8-json_api.py <letter>
 """
 import sys
 import requests
 
-
 if __name__ == "__main__":
-    value = {"q": ""}
-    if len(sys.argv[1]) > 1:
-        value["q"] = sys.argv[1]
-
-    resp = requests.post("http://0.0.0.0:5000/search_user", data=value)
-    try:
-        resp = resp.json()
-        if resp == {}:
-            print("No result")
-        else:
-            print(f"[{resp.get('id')}] {resp.get('name')}")
-    except ValueError:
-        print("Not a valid JSON")
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    resp = requests.get("https://api.github.com/user", auth=auth)
+    print(resp.json().get("id"))
