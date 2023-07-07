05d5ca05ecdd9f2b2fb65800503e9752fa7e2266
BinyamMamo <binyammamo01@gmail.com>
2023-07-07 19:21:30 +0300

    Fri Jul  7 19:21:30 EAT 2023

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,2 @@
 -- computes the average of all records --
-ALTER TABLE second_table ADD average INT;
-UPDATE second_table SET average = AVG(score) *;
+SELECT AVG(score) AS "average" FROM second_table;
