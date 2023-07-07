b7c23342f7767fb3dcf8962ae7e37fdd81411472
BinyamMamo <binyammamo01@gmail.com>
2023-07-07 19:34:17 +0300

    Fri Jul  7 19:34:17 EAT 2023

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- lists all records --
-SELECT score, name FROM second_table WHERE name NOT NULL ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
