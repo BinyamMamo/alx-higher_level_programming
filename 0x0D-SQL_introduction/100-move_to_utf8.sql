4f25ad2cbc54189a7efd59d8bfa574424c527816
BinyamMamo <binyammamo01@gmail.com>
2023-07-08 19:43:35 +0300

    Sat Jul  8 19:43:34 EAT 2023

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,2 +1,2 @@
 -- converts the database to UTF-8 --
-ALTER TABLE `first_table` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE `hbtn_0c_0` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
