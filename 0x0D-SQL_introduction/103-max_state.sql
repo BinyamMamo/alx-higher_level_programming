a35d748da2559002c2c1c424b4d80e1c6b474f74
BinyamMamo <binyammamo01@gmail.com>
2023-07-09 12:08:09 +0300

    Sun Jul  9 12:08:09 EAT 2023

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,2 +1,4 @@
 -- displays max temp of the sates in order --
-SELECT `state`, MAX(`value`) AS `max_temp` FROM `temperatures` GROUP BY `state` ORDER BY `state` ASC;  
+SELECT `state`, MAX(`value`) AS `max_temp` FROM `temperatures` 
+    GROUP BY `state` 
+    ORDER BY `state` ASC;  
