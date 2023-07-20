14c2d97cbeba42c1ba33940e5d29ead5e1f596a2
BinyamMamo <binyammamo01@gmail.com>
2023-07-20 16:30:23 +0300

    Thu Jul 20 16:30:23 EAT 2023

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,7 +2,7 @@
 DROP USER IF EXISTS user_0d_1@localhost;
 CREATE USER user_0d_1@localhost
     IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL
-   ON *.*
+GRANT PROXY
+   ON root
    TO user_0d_1@localhost;
 FLUSH PRIVILEGES;
