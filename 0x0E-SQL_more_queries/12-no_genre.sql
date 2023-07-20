09ce8642224c58653a472acd3473d5744dbd2fb1
BinyamMamo <binyammamo01@gmail.com>
2023-07-20 11:41:56 +0300

    Thu Jul 20 11:41:56 EAT 2023

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,7 +1,7 @@
 -- List shows without genre using LEFT JOIN statement
 SELECT ts.title, tsg.genre_id
 FROM tv_shows ts
-JOIN tv_show_genres tsg
+LEFT JOIN tv_show_genres tsg
     ON ts.id = tsg.show_id
 WHERE tsg.genre_id IS NULL
 ORDER BY ts.title, tsg.genre_id;
