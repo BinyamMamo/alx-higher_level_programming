f98b4101a1191304a39d1e74703ad28fba88bd1d
BinyamMamo <binyammamo01@gmail.com>
2023-07-20 16:53:16 +0300

    Thu Jul 20 16:53:16 EAT 2023

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,8 +1,3 @@
--- Create user user_0d_1 with all privileges --
-DROP USER IF EXISTS user_0d_1@localhost;
-CREATE USER user_0d_1@localhost
-    IDENTIFIED BY 'user_0d_1_pwd';
-GRANT PROXY
-   ON root
-   TO user_0d_1@localhost;
-FLUSH PRIVILEGES;
+-- Create user user_0d_1 with all privileges
+CREATE USER IF NOT EXISTS user_0d_1 @localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1 @localhost;FLUSH PRIVILEGES;
