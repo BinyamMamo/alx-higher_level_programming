783476da2bdc5cdddb5975ba19a799a0b5e52682
BinyamMamo <binyammamo01@gmail.com>
2023-07-20 15:04:26 +0300

    Thu Jul 20 15:04:26 EAT 2023

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,1 +1,12 @@
 -- List all shows with a score greater or equal to 8.5 in the database hbtn_0d_tvshows
+SELECT 
+    title
+FROM tv_shows
+WHERE id NOT IN (
+    SELECT 
+        tsg.show_id
+    FROM tv_show_genres tsg
+    JOIN tv_genres tg
+        ON tsg.genre_id = tg.id
+    WHERE tg.name != 'Comedy'
+);
