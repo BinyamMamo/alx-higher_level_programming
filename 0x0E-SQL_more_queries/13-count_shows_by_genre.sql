78b6aa95c7defde13bcd341d60389aeda7c8eec9
BinyamMamo <binyammamo01@gmail.com>
2023-07-20 12:12:59 +0300

    Thu Jul 20 12:12:59 EAT 2023

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,1 +1,6 @@
 -- List genres with number of shows using JOIN statement
+SELECT
+    name AS 'genre',
+    (SELECT COUNT(genre_id) FROM tv_show_genres WHERE tv_genres.id = genre_id) AS 'number_of_shows'
+FROM tv_genres
+ORDER BY number_of_shows DESC;
