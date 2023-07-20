a0f2dccf0c1047cf15057d8514f63ecdd4192eca
BinyamMamo <binyammamo01@gmail.com>
2023-07-20 10:15:17 +0300

    Thu Jul 20 10:15:17 EAT 2023

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,5 +1,5 @@
 -- Create table force_name with name not null --
-CREATE TABLE IF NOT EXISTS 'force_name' (
-    'id' INT,
-    'name' VARCHAR(256) NOT NULL
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL
 );
