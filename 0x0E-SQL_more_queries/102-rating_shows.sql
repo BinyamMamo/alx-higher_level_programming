64aab273226b42a86ed28ecc2b1a2044a5f1cd8c
BinyamMamo <binyammamo01@gmail.com>
2023-07-20 15:51:48 +0300

    Thu Jul 20 15:51:48 EAT 2023

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -4,6 +4,6 @@ SELECT
     SUM(rate) AS rating
 FROM tv_shows AS ts
     INNER JOIN tv_show_ratings AS tsr
-    ON t.id = r.show_id
+    ON ts.id = trs.show_id
 GROUP BY title
 ORDER BY rating DESC;
