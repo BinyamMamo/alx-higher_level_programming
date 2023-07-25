8511e3dfae03c987f516de552ea56db3afd94354
BinyamMamo <117805811+BinyamMamo@users.noreply.github.com>
2023-07-25 20:54:34 +0000

    Tue Jul 25 20:54:34 UTC 2023

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,18 +1,17 @@
 #!/usr/bin/node
-const fs = require('fs');
 const request = require('request');
 const url = process.argv[2] + '?completed=true';
 
 request(url, (err, resp, body) => {
   if (err) { console.log(err); return; }
   const todos = JSON.parse(body);
-  let todo_dict = {};
+  const todoDict = {};
   for (const todo of todos) {
-    if (todo.userId in todo_dict) {
-      todo_dict[todo.userId] += 1;
+    if (todo.userId in todoDict) {
+      todoDict[todo.userId] += 1;
     } else {
-      todo_dict[todo.userId] = 1;
+      todoDict[todo.userId] = 1;
     }
   }
-  console.log(todo_dict);
+  console.log(todoDict);
 });
