3fd743ade225e429d05835f8c52d18af640c9a1c
BinyamMamo <117805811+BinyamMamo@users.noreply.github.com>
2023-07-25 21:45:03 +0000

    Tue Jul 25 21:45:03 UTC 2023

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
+/* makes a request to the Star Wars API (SWAPI) 
+to retrieve information about a specific movie. */
 const request = require('request');
 const movieId = process.argv[2];
-const url = "https://swapi-api.alx-tools.com/api/films/" + movieId;
+const url = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 
 request(url, (err, resp, body) => {
   if (err) { console.log(err); return; }
