0158be35ccf9fbbf72604a01adebacff3814e647
BinyamMamo <117805811+BinyamMamo@users.noreply.github.com>
2023-07-25 22:05:40 +0000

    Tue Jul 25 22:05:40 UTC 2023

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -5,7 +5,7 @@
  * creates a dictionary called `todoDict`
  */
 const request = require('request');
-const url = process.argv[2];
+const url = process.argv[2] + '/';
 
 request(url, (err, resp, body) => {
   if (err) { console.log(err); return; }
