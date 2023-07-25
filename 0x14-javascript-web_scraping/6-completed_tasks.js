ecce913feb77d5ad456587fcb9211c8fc252406b
BinyamMamo <117805811+BinyamMamo@users.noreply.github.com>
2023-07-25 21:00:10 +0000

    Tue Jul 25 21:00:10 UTC 2023

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -5,7 +5,7 @@ const url = process.argv[2];
 request(url, (err, resp, body) => {
   if (err) { console.log(err); return; }
   const todos = JSON.parse(body);
-  const todoDict = {};
+  let todoDict = {};
   for (const todo of todos) {
     if (todo.completed) {
       if (todo.userId in todoDict) {
