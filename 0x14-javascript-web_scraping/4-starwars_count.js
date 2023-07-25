57bd6bec1b5aff06d4c790a95992e57a38d2b2fd
BinyamMamo <117805811+BinyamMamo@users.noreply.github.com>
2023-07-25 20:34:45 +0000

    Tue Jul 25 20:34:45 UTC 2023

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,7 +4,6 @@ const url = process.argv[2];
 request(url, (err, resp, body) => {
   if (err) { console.log(err); return; }
   let count = 0;
-  console.log(JSON.parse(body).results[0].characters);
   const results = JSON.parse(body).results;
   for (const film of results) {
     for (const character of film.characters) {
