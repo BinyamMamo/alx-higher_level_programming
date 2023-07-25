65be8d2035a837c275ec9d1c56d6099ddf7795a9
BinyamMamo <117805811+BinyamMamo@users.noreply.github.com>
2023-07-25 20:33:45 +0000

    Tue Jul 25 20:33:45 UTC 2023

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,18 @@
 #!/usr/bin/node
 const request = require('request');
-const url = process.argv[2].replace('films', 'people') + '18';
+const url = process.argv[2];
 request(url, (err, resp, body) => {
   if (err) { console.log(err); return; }
-  console.log(JSON.parse(body).films.length);
+  let count = 0;
+  console.log(JSON.parse(body).results[0].characters);
+  const results = JSON.parse(body).results;
+  for (const film of results) {
+    for (const character of film.characters) {
+      if (character.includes('18')) {
+        ++count;
+      }
+    }
+  }
+
+  console.log(count);
 });
