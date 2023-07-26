89435ebf9fe4f7a041cc4d395047ad4f4336d150
BinyamMamo <binyammamo01@gmail.com>
2023-07-26 12:42:00 +0300

    Wed Jul 26 12:41:59 EAT 2023

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Establishes a connection to a MySQL database
+"""
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    conn = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
+                           password=argv[2], database=argv[3])
+    db = conn.cursor()
+    db.execute("SELECT * FROM states WHERE name like 'N*' ORDER BY id;")
+    data = db.fetchall()
+    for item in data:
+        print(item)
+    conn.close()
