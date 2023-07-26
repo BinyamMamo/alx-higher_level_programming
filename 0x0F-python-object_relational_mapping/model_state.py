6df8d0fe369297b93523f066112a4f25119201e2
BinyamMamo <binyammamo01@gmail.com>
2023-07-26 17:53:32 +0300

    Wed Jul 26 17:53:32 EAT 2023

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -3,16 +3,18 @@
 Contains a `State` class, a SQLAlchemy model that represents
 a table called "states", with columns for id and name.
 """
-from sqlalchemy import create_engine, Column, Integer, String, MetaData
-from sqlAlchemy.ext.declarative import declarative_base
 
-meta = MetaData()
-Base = declarative_base(metadata=meta)
+if __name__ == __main__:
+    from sqlalchemy import create_engine, Column, Integer, String, MetaData
+    from sqlAlchemy.ext.declarative import declarative_base
 
-class State(Base):
-    """
-    Defines a SQLAlchemy model class called `State`
-    """
-    __tablename__ = "states"
-    id = Column(Integer, unique=True, nullable=False, primary_key=True)
-    name = Column(String(128), nullable=False)
+    meta = MetaData()
+    Base = declarative_base(metadata=meta)
+
+    class State(Base):
+        """
+        Defines a SQLAlchemy model class called `State`
+        """
+        __tablename__ = "states"
+        id = Column(Integer, unique=True, nullable=False, primary_key=True)
+        name = Column(String(128), nullable=False)
