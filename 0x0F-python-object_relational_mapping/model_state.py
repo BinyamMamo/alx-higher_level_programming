af02fa53dd7e553df7c878768ff5684be108faff
BinyamMamo <binyammamo01@gmail.com>
2023-07-26 18:30:20 +0300

    Wed Jul 26 18:30:20 EAT 2023

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -11,14 +11,9 @@ if __name__ == "__main__":
     meta = MetaData()
     Base = declarative_base(metadata=meta)
 
+    """Defines a SQLAlchemy model class called `State`
+    """
     class State(Base):
-        """Defines a SQLAlchemy model class called `State`
-
-        Args:
-            Base (declarative_base): includes base
-        """
-
-
         __tablename__ = "states"
         id = Column(Integer, unique=True, nullable=False, primary_key=True)
         name = Column(String(128), nullable=False)
