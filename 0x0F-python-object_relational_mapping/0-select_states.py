6016df69cac3ddf73c2957ca1b8bd71a911a7cb4
BinyamMamo <binyammamo01@gmail.com>
2023-07-26 12:36:02 +0300

    Wed Jul 26 12:36:02 EAT 2023

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -12,5 +12,6 @@ if __name__ == "__main__":
     db = conn.cursor()
     db.execute("SELECT * FROM states;")
     data = db.fetchall()
-    print(data)
+    for item in data:
+        print(item)
     conn.close()
