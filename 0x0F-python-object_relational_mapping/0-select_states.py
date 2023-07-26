b96578b12d0e3ed7c302f7dd9ca16c974c3d48a9
BinyamMamo <binyammamo01@gmail.com>
2023-07-26 12:30:37 +0300

    Wed Jul 26 12:30:37 EAT 2023

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,10 +8,7 @@ if __name__ == "__main__":
     from sys import argv
 
 
-    username = argv[1]
-    pwd = argv[2]
-    dbname = argv[3]
-    conn = MySQLdb.connect(host='localhost', port=3306, user=username, password=pwd, database=dbname)
+    conn = MySQLdb.connect(host='localhost:3306', user=argv[1], password=argv[2], database=argv[3])
     db = conn.cursor()
     db.execute("SELECT * FROM states;")
     data = db.fetchAll()
