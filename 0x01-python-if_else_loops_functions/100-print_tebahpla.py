8d718934ad595dcd5b32d90ed5f2bd9bec2aa689
BinyamMamo <binyammamo01@gmail.com>
2023-07-27 10:15:59 +0300

    Thu Jul 27 10:15:59 EAT 2023

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 for i in range(25, -1, -1):
-    print("{}".format(chr(i + ord('a') if (i % 2) else ord('A'))), end='')
+    print("{}".format(chr(i + (ord('a') if (i % 2) else ord('A')))), end='')
