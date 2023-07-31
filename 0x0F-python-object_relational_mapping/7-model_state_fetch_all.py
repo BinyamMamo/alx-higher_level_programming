a95e536461be7118b301815ce1f9b7f08a1a70a4
BinyamMamo <binyammamo01@gmail.com>
2023-07-31 11:40:17 +0300

    Mon Jul 31 11:40:17 EAT 2023

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,19 +1,19 @@
-#!/usr/bin/python3
 """
-Creates a connection to a MySQL database using SQLAlchemy in Python
+Establishes a connection to a MySQL database and
+retrieves data from a table called `State`
 """
-from model_state import Base, State
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sys import argv
-
 if __name__ == "__main__":
-    #                                                                      #user      #pass    #db
-    engine = create_engine("mysql://{}:{}@localhost:3306/{}".format(argv[1], argv[2], argv[3]))
-
-    Base.metadata.create_all(bind=engine)
-    Session = sessionmaker(bind=engine)
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from sys import argv
+    from pprint import pprint
+    #                                       user pass            db
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}\
+                           ".format(argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(engine)
     session = Session()
-    result = session.query(State).all()
-    for data in result:
-        print(result)        
+    data = session.query(State).order_by(State.id).all()
+    for d in data:
+        print(d.id, ": ", d.name, sep="")
