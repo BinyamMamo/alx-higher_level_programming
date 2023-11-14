02c10806dc6d8fed637b5bf555e1539b3db9a536
BinyamMamo <binyammamo01@gmail.com>
2023-11-14 20:40:12 +0400

    Tue Nov 14 20:40:12 +04 2023

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -4,4 +4,4 @@ const fs = require('fs');
 const file1 = fs.readFileSync(`${process.argv[2]}`, 'utf-8');
 const file2 = fs.readFileSync(`${process.argv[3]}`, 'utf-8');
 
-fs.writeFileSync(`${process.argv[4]}`, file1 + file2);
+fs.writeFileSync(process.argv[4], file1 + file2);
