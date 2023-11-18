189562a814ac603aea681d2f8715260d04e34152
BinyamMamo <binyammamo01@gmail.com>
2023-11-18 20:19:18 +0400

    Sat Nov 18 20:19:18 +04 2023

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -3,6 +3,7 @@
 Connects to a MySQL database and executes a query to
 retrieve data from a table called "states"
 """
+
 if __name__ == "__main__":
     import MySQLdb
     from sys import argv
@@ -10,7 +11,7 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
                            password=argv[2], database=argv[3])
     db = conn.cursor()
-    db.execute("SELECT * FROM states WHERE name LIKE BINARY '%s';",
+    db.execute("SELECT * FROM states WHERE name LIKE BINARY '%s'",
                (argv[4], ))
     data = db.fetchall()
     for item in data:
