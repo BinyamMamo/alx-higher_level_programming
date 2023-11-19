ce40dc204ac1ded504c1ade71cce18c520da9630
BinyamMamo <binyammamo01@gmail.com>
2023-11-19 07:28:38 +0400

    Sun Nov 19 07:28:37 +04 2023

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -3,17 +3,19 @@
 Establishes a connection to a MySQL database using the provided
 command line arguments and fetches the first row of the States stable
 """
-if __name__ == "__main__":
-    from model_state import Base, State
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import sessionmaker
-    from sys import argv
 
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+if __name__ == "__main__":
     engine = create_engine("""mysql+mysqldb://{}:{}@localhost:
                            3306/{}""".format(argv[1], argv[2], argv[3]))
     Session = sessionmaker(engine)
     session = Session()
-    data = session.query(State).order_by(State.id).filter(State.id == 7).first()
+    data = session.query(State).order_by(State.id)
+    data = data.filter(State.id == 7).first()
     if data is not None:
         print(data.id, ": ", data.name, sep="")
     else:
