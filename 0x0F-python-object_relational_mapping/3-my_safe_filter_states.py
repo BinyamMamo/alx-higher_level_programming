5b69e0e0bdb69ffcbab2a5ff87be6060ff7fcd8a
BinyamMamo <binyammamo01@gmail.com>
2023-11-19 07:25:43 +0400

    Sun Nov 19 07:25:43 +04 2023

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -3,19 +3,16 @@
 Connects to a MySQL database and executes a query to
 retrieve data from a table called "states"
 """
-if __name__ == "__main__":
-    import MySQLdb
-    from sys import argv
 
-    conn = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
-                           password=argv[2], database=argv[3])
-    db = conn.cursor()
-    query = """SELECT * FROM states
-                        WHERE states.name LIKE BINARY '{}'
-                        ORDER BY id;"""
-    db.execute(query.format(argv[4]))
-    data = db.fetchall()
-    for item in data:
-        print(item)
-    db.close()
-    conn.close()
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
+    for state in rows:
+        if state[1] == argv[4]:
+            print(state)
