9350ac5131ce556278ea63e2c540f3e9481e12ae
BinyamMamo <binyammamo01@gmail.com>
2023-11-19 08:13:02 +0400

    Sun Nov 19 08:13:02 +04 2023

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     from relationship_state import State
     from relationship_city import City
 
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
                            argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
     Session = sessionmaker(engine)
