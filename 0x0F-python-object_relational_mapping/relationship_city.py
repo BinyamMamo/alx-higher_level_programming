8547e321131c021a3cd13a233fe439de0dd2e0b6
BinyamMamo <binyammamo01@gmail.com>
2023-11-19 07:56:00 +0400

    Sun Nov 19 07:56:00 +04 2023

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Defines the City class for representing cities in the database.
+"""
+
+from lib2to3.pytree import Base
+from sre_parse import State
+from unicodedata import name
+from sqlalchemy import Column, ForeignKey, Integer, String, null
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class City(Base):
+    """
+    Represents a city in the database.
+    """
+
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
