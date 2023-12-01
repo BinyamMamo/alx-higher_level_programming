df9f22117a9ce674895574fead32dc22737db07d
BinyamMamo <binyammamo01@gmail.com>
2023-12-01 20:53:53 +0400

    Fri Dec  1 20:53:53 +04 2023

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
         value["q"] = sys.argv[1]
     resp = requests.post("http://0.0.0.0:5000/search_user", data=value).json()
     try:
-        if resp == {}:
+        if resp == {} or resp is None:
             print("No result")
         else:
             print(f"[{resp.get('id')}] {resp.get('name')}")
