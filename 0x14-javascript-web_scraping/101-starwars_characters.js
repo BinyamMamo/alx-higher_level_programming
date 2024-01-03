c502a8e2325cc97dc5fd38e3297af6b679e58e89
BinyamMamo <binyammamo01@gmail.com>
2024-01-04 01:26:06 +0400

    Thu Jan  4 01:26:06 +04 2024

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -4,6 +4,8 @@
  * to retrieve information about a specific movie.
  */
 const request = require('request');
+const async = require('async');
+
 const movieId = process.argv[2];
 const url = 'https://swapi-api.alx-tools.com/api/films/' + movieId + '/';
 
@@ -11,10 +13,14 @@ request(url, (err, resp, body) => {
   if (err) { console.log(err); return; }
 
   const characters = JSON.parse(body).characters;
-  for (const character of characters) {
+
+  async.eachSeries(characters, (character, callback) => {
     request(character, (e, res, b) => {
       if (e) { console.log(e); return; }
       console.log(JSON.parse(b).name);
+      callback();
     });
-  }
+  }, (err) => {
+    if (err) { console.log(err); return; }
+  });
 });
